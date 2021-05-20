a845990b1bacf975f13f28341ea469ee02909f3e
Alex Bie <alexbie98@gmail.com>
2021-05-20 15:52:48 -0400

    remove template specialization declarations in Ops.h

--- src/geom/Ops.cc
+++ src/geom/Ops.cc
@@ -1,6 +1,7 @@
 #include "Ops.h"
 
 using namespace glm;
+using namespace std;
 
 dmat4 translate(const dvec3& x){
     dmat4 mat;
@@ -11,4 +12,22 @@ dmat4 translate(const dvec3& x){
 dmat4 scale(const dvec3& s){
     dmat4 mat;
     return mat;
+}
+
+template<>
+dmat4 rot<X>(double angle) {
+    cout << "X" << endl;
+    return dmat4{};
+}
+
+template<>
+dmat4 rot<Y>(double angle) {
+    cout << "Y" << endl;
+    return dmat4{};
+}
+
+template<>
+dmat4 rot<Z>(double angle) {
+    cout << "Z" << endl;
+    return dmat4{};
 }
--- src/geom/Ops.h
+++ src/geom/Ops.h
@@ -16,35 +16,5 @@ glm::dmat4 translate(const glm::dvec3& x);
 
 glm::dmat4 scale(const glm::dvec3& s);
 
-// template <Direction d, std::enable_if_t<d==X> * = nullptr>
-// glm::dmat4 rot(double angle);
-
-// template <Direction d, std::enable_if_t<d==Y> * = nullptr>
-// glm::dmat4 rot(double angle);
-
-// template <Direction d, std::enable_if_t<d==Z> * = nullptr>
-// glm::dmat4 rot(double angle);
-
-template<Direction d> 
-inline glm::dmat4 rot(double angle) {
-    std::cout << d << std::endl;
-    return glm::dmat4{};
-}
-
-template<>
-inline glm::dmat4 rot<X>(double angle) {
-    std::cout << "X" << std::endl;
-    return glm::dmat4{};
-}
-
-template<>
-inline glm::dmat4 rot<Y>(double angle) {
-    std::cout << "Y" << std::endl;
-    return glm::dmat4{};
-}
-
-template<>
-inline glm::dmat4 rot<Z>(double angle) {
-    std::cout << "Z" << std::endl;
-    return glm::dmat4{};
-}
+template <Direction d>
+glm::dmat4 rot(double angle);
